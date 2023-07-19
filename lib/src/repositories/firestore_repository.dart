54f183a33a1a823b90642c62818a04739cc39602
jjpp43 <jparkdev63@gmail.com>
2023-07-19 16:24:31 +0900

    + Function to get initial 20 posts. + Function to get posts to start off from where it ended

--- lib/src/repositories/firestore_repository.dart
+++ lib/src/repositories/firestore_repository.dart
@@ -3,12 +3,33 @@ import 'package:hotspot/src/models/info_model.dart';
 
 class FirestoreRepository {
   //Get info
-  static Future<List<InfoModel>> getInfoLists() async {
+  static Future<List<InfoModel>> getInfoLists({int limit = 20}) async {
     List<InfoModel> infoList = [];
 
     try {
-      final data =
-          await FirebaseFirestore.instance.collection('info').limit(16).get();
+      final data = await FirebaseFirestore.instance
+          .collection('info')
+          .limit(limit)
+          .get();
+      for (var info in data.docs) {
+        infoList.add(InfoModel.fromMap(info.data()));
+      }
+      return infoList;
+    } catch (e) {
+      throw (Exception(e.toString()));
+    }
+  }
+
+  static Future<List<InfoModel>> getMoreInfoLists(
+      {required InfoModel lastInfo, int limit = 20}) async {
+    List<InfoModel> infoList = [];
+
+    try {
+      final data = await FirebaseFirestore.instance
+          .collection('info')
+          .startAfter([lastInfo.toMap()])
+          .limit(limit)
+          .get();
       for (var info in data.docs) {
         infoList.add(InfoModel.fromMap(info.data()));
       }
